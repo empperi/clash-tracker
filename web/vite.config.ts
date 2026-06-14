0e6a78f3ba32ca7b3bcf86f3d7052c6392189da2
Niklas Collin <niklas.collin@gmail.com>
2026-06-14 13:00:15 +0300

    chore: add server config for vite to allow access from mobile device during development

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,10 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+    port: 5173
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
